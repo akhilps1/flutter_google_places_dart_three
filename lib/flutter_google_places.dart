9796b0f5ba0f7896553d8d8df9a53b38e74e6537
akhilps12 <akhil@lilacinfotech.com>
2024-09-12 15:55:10 +0530

    .

--- lib/flutter_google_places.dart
+++ lib/flutter_google_places.dart
@@ -1,5 +1,7 @@
-library flutter_google_places;
+library flutter_google_places_dart_three;
 
 export 'src/flutter_google_places.dart';
 export 'src/places_autocomplete_field.dart';
 export 'src/places_autocomplete_form_field.dart';
+export 'package:google_api_headers/google_api_headers.dart';
+export 'package:google_maps_webservice_dart_three/places.dart';
